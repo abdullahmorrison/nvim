51b5bbd9cd4f810ff7ac4ddd995b1fe2b3f9064a
abdullahmorrison <abdullahmorrison@gmail.com>
2023-09-02 08:51:17 -0400

    remap: yank functionality

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,2 +1,13 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Shorten function name
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>e", vim.cmd.Ex)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv^", opts)
+keymap("v", ">", ">gv^", opts)
+
+keymap("v", "p", '"_dP', opts) -- yank holds the same value after pasting over some text
