736e772e64285e9171bc9fdfd1ae72f7c17e2ac7
abdullahmorrison <abdullahmorrison@gmail.com>
2023-09-19 18:20:11 -0400

    color scheme load error message update

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,6 +2,6 @@ local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-    vim.notify("colorshceme" .. colorscheme .. "not found!")
+    vim.notify("colorshceme " .. colorscheme .. " not found!")
     return
 end
