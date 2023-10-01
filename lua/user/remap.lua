2eee16c0f9e6e974b3bc430499b746cb3d06b6eb
abdullahmorrison <abdullahmorrison@gmail.com>
2023-10-01 13:15:17 -0400

    remap: better window nav + resize window

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -20,9 +20,19 @@ local keymap = vim.keymap.set
 --   visual_block_mode = "x",
 --   term_mode = "t",
 --   command_mode = "c",
+--
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
--- explorer to the left
-keymap("n", "<leader>e", ":Lex 10 <cr>", opts)
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Visual --
 -- Stay in indent mode
@@ -30,3 +40,6 @@ keymap("v", "<", "<gv^", opts)
 keymap("v", ">", ">gv^", opts)
 
 keymap("v", "p", '"_dP', opts) -- yank holds the same value after pasting over some text
+
+-- explorer to the left
+keymap("n", "<leader>e", ":Lex 10 <cr>", opts)
