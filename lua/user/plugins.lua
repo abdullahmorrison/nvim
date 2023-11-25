6a42bf484bb69dc6af4707d2095af56ffb1313e6
abdullahmorrison <abdullahmorrison@gmail.com>
2023-11-25 09:08:11 -0500

    added dressing.nvim for better cmd line input UI

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -43,6 +43,7 @@ return packer.startup(function(use)
     use "wbthomason/packer.nvim"             -- Have packer manage itself
     use "nvim-lua/plenary.nvim"              -- Useful lua functions used by lots of plugins
     use "rebelot/kanagawa.nvim"              -- colorscheme
+    use "stevearc/dressing.nvim"             -- improve cmd line input UI
 
     -- code completion
     use  "hrsh7th/nvim-cmp"                  -- The completion plugin
