827e5ed5d97e8dc5975803281a98e4c8862deeb1
abdullahmorrison <abdullahmorrison@gmail.com>
2023-12-09 09:47:53 -0500

    update copilot config

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -12,6 +12,7 @@ vim.g.maplocalleader = " "
 
 --Copilot
 vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 -- Shorten function name
