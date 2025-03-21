b84b47bd49efbe8f0ba798c115fdc5deed3115e0
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-03-21 14:49:14 -0400

    added copilot and copilot chat #36

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  { "github/copilot.vim" },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+  }
+}
